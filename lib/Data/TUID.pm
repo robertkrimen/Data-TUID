97f133b3c34e5f56ce6f48b5693b5dce055d2db5
robertkrimen <robertkrimen@gmail.com>
2010-09-10 14:05:58 -0700

    Using dzpl for building

--- lib/Data/TUID.pm
+++ lib/Data/TUID.pm
@@ -1,19 +1,8 @@
 package Data::TUID;
+# ABSTRACT: A smaller and more communicable pseudo-UUID
 
 use warnings;
 use strict;
-
-=head1 NAME
-
-Data::TUID - A smaller and more communicable pseudo-UUID
-
-=head1 VERSION
-
-Version 0.011
-
-=cut
-
-our $VERSION = '0.011';
 
 =head1 SYNOPSIS
 
@@ -140,60 +129,8 @@ sub tuid {
     return $tuid;
 }
 
-=head1 AUTHOR
-
-Robert Krimen, C<< <rkrimen at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-data-tuid at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Data-TUID>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Data::TUID
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Data-TUID>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Data-TUID>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Data-TUID>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Data-TUID/>
-
-=back
-
-
 =head1 ACKNOWLEDGEMENTS
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 Robert Krimen, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
 
 =cut
 
-1; # End of Data::TUID
+1;
